734fec4777502b2adc4b5d129c870dc4f5309b60
remus00 <135813551+remus00@users.noreply.github.com>
2023-11-28 22:23:25 +0100

    Leftbar and complete Routing

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { Link, NavLink, useNavigate, useLocation } from 'react-router-dom';
+import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations';
+import { useUserContext } from '@/context/AuthContext';
+import Logo from '../../../public/assets/images/logo.svg';
+import ProfileImgPlaceholder from '../../../public/assets/images/profile.png';
+import LogOutIcon from '../../../public/assets/icons/logout.svg';
+import { sidebarLinks } from '@/constants';
+import { INavLink } from '@/types';
+import { Button } from '../ui/button';
 
 const LeftSidebar = () => {
-    return <div>LeftSidebar</div>;
+    const { pathname } = useLocation();
+    const { mutate: signOut, isSuccess } = useSignOutAccount();
+    const navigate = useNavigate();
+    const { user } = useUserContext();
+
+    useEffect(() => {
+        if (isSuccess) navigate(0);
+    }, [isSuccess]);
+
+    return (
+        <nav className="leftsidebar">
+            <div className="flex flex-col gap-11">
+                <Link to="/" className="flex gap-3 items-center">
+                    <img src={Logo} alt="logo" width={170} height={36} />
+                </Link>
+
+                <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+                    <img
+                        src={user.imageUrl || ProfileImgPlaceholder}
+                        alt="profile-image"
+                        className="h-14 w-14 rounded-full"
+                    />
+
+                    <div className="flex flex-col">
+                        <p className="body-bold">{user.name}</p>
+                        <p className="small-regular text-light-3">@{user.username}</p>
+                    </div>
+                </Link>
+
+                <ul className="flex flex-col gap-6">
+                    {sidebarLinks.map((link: INavLink) => {
+                        const isActive = pathname === link.route;
+
+                        return (
+                            <li
+                                className={`leftsidebar-link group  ${
+                                    isActive && 'bg-primary-500'
+                                }`}
+                                key={link.label}
+                            >
+                                <NavLink to={link.route} className="flex gap-4 items-center p-4">
+                                    <img
+                                        src={link.imgURL}
+                                        alt={link.label}
+                                        className={`group-hover:invert-white ${
+                                            isActive && 'invert-white'
+                                        }`}
+                                    />
+                                    {link.label}
+                                </NavLink>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
+                <img src={LogOutIcon} alt="logout" />
+                <p className="small-medium lg:base-medium">Logout</p>
+            </Button>
+        </nav>
+    );
 };
 
 export default LeftSidebar;
--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -20,7 +20,7 @@ const Topbar = () => {
         <section className="topbar">
             <div className="flex-between py-4 px-5">
                 <Link to="/" className="flex gap-3 items-center">
-                    <img src={Logo} alt="logo" width={130} height={325} />
+                    <img src={Logo} alt="logo" width={130} height={32} />
                 </Link>
 
                 <div className="flex gap-4">
